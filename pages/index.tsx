77d2418cd237a7be13ab98620d93d480a3c5f670
infrasenseai-art <infrasenseai@gmail.com>
2025-10-02 14:21:19 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -313,3 +313,20 @@ export default function Home() {
     </div>
   );
 }
+import { Resend } from 'resend';
+const resend = new Resend('re_123456789');
+
+(async function() {
+  try {
+    const data = await resend.emails.send({
+      from: 'Acme <onboarding@resend.dev>',
+      to: ['delivered@resend.dev'],
+      subject: 'Hello World',
+      html: '<strong>It works!</strong>'
+    });
+
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+})();
