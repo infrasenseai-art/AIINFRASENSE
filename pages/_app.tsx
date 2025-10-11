ae16dfd789f9ae71d722fd06fb3e4ae8dbd45584
infrasenseai-art <infrasenseai@gmail.com>
2025-10-11 11:09:07 +0200

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,4 +5,4 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
-import { Analytics } from "@vercel/analytics/next"
+
