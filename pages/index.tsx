7c3f058efe14d6cefa04ecda2f9b484e0c608eb9
infrasenseai-art <infrasenseai@gmail.com>
2025-11-04 16:51:22 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -394,16 +394,4 @@ export default function Home() {
     </div>
   );
 }
-import ChatWidget from "@/components/ChatWidget";
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="de">
-      <body>
-        {children}
-        <ChatWidget />
-      </body>
-    </html>
-  );
-}
 
