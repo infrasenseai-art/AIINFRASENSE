1af2991ff2113ab4f04c8ff069a51f5c6bd61cf0
infrasenseai-art <infrasenseai@gmail.com>
2025-11-04 18:39:56 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,13 @@
 // pages/_app.tsx
-import type { AppProps } from 'next/app';
-import '../styles/globals.css';
-import { Analytics } from '@vercel/analytics/react';
+import type { AppProps } from "next/app";
+import ChatWidget from "./ChatWidget"; // liegt bei dir in /pages
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-      <Analytics />
+      <ChatWidget />
     </>
   );
 }
+
