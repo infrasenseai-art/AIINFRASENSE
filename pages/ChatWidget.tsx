17efc8f209932a99ce30cb351ad74436a7f49d3b
infrasenseai-art <infrasenseai@gmail.com>
2025-11-04 16:48:02 +0100

    Update ChatWidget.tsx

--- pages/ChatWidget.tsx
+++ pages/ChatWidget.tsx
@@ -1,1 +1,12 @@
-<script src='https://widget.superchat.de/snippet.js?applicationKey=WC0J3WbML4zEXX80EGZ6DON2y5' referrerpolicy='no-referrer-when-downgrade'></script>
+import Script from "next/script";
+
+export default function ChatWidget() {
+  return (
+    <Script
+      id="superchat-widget"
+      src="https://widget.superchat.de/snippet.js?applicationKey=WC0J3WbML4zEXX80EGZ6DON2y5"
+      strategy="afterInteractive"
+      referrerPolicy="no-referrer-when-downgrade"
+    />
+  );
+}
